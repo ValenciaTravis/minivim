ce3d715edc04ba2e2982d1ac2b04e1271df5c879
ValenciaTravis <jackshliu@sjtu.edu.cn>
2025-12-29 03:50:43 +0800

    modify readme.md

---
+++ build/CMakeFiles/minivim.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/e/minivim/src/cursor.cpp" "/mnt/e/minivim/build/CMakeFiles/minivim.dir/src/cursor.cpp.o"
+  "/mnt/e/minivim/src/main.cpp" "/mnt/e/minivim/build/CMakeFiles/minivim.dir/src/main.cpp.o"
+  "/mnt/e/minivim/src/multiTrie.cpp" "/mnt/e/minivim/build/CMakeFiles/minivim.dir/src/multiTrie.cpp.o"
+  "/mnt/e/minivim/src/myApplication.cpp" "/mnt/e/minivim/build/CMakeFiles/minivim.dir/src/myApplication.cpp.o"
+  "/mnt/e/minivim/src/myFile.cpp" "/mnt/e/minivim/build/CMakeFiles/minivim.dir/src/myFile.cpp.o"
+  "/mnt/e/minivim/src/myWindow.cpp" "/mnt/e/minivim/build/CMakeFiles/minivim.dir/src/myWindow.cpp.o"
+  "/mnt/e/minivim/src/utils.cpp" "/mnt/e/minivim/build/CMakeFiles/minivim.dir/src/utils.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/minivim.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "../bin/minivim"
+  "../bin/minivim.pdb"
+  "CMakeFiles/minivim.dir/src/cursor.cpp.o"
+  "CMakeFiles/minivim.dir/src/main.cpp.o"
+  "CMakeFiles/minivim.dir/src/multiTrie.cpp.o"
+  "CMakeFiles/minivim.dir/src/myApplication.cpp.o"
+  "CMakeFiles/minivim.dir/src/myFile.cpp.o"
+  "CMakeFiles/minivim.dir/src/myWindow.cpp.o"
+  "CMakeFiles/minivim.dir/src/utils.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/minivim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
